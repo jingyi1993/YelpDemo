b9f89d2f39d052153d3049a727aacf7d8b5ed38b
马静怡 <472382917@qq.com>
2019-11-15 14:13:13 -0500

    config to carrierwave model(add location info)

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,11 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     'AKIAJHIE5PVHK473S2DA',                        # required unless using use_iam_profile
-    aws_secret_access_key: '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',                        # required unless using use_iam_profile
-    region:                'us-east-1',                  # optional, defaults to 'us-east-1'
-    # host:                  's3-us-east-1.amazonaws.com',
+    provider:              'AWS',
+    aws_access_key_id:     'AKIAJHIE5PVHK473S2DA',
+    aws_secret_access_key: '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',
+    region:                'ca-central-1',
   }
   config.fog_directory  = 'awsmaggie'
-  config.fog_public     = true                                                 # optional, defaults to true
-  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }                                      # required
+  config.fog_public     = true
+  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
 end
