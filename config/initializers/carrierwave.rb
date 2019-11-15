53fb3f5aa197ca8ab60d9c2d86956048a5df8a31
马静怡 <472382917@qq.com>
2019-11-15 14:19:41 -0500

    use cresdential way to save id and key for aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:              'AWS',
-    aws_access_key_id:     'AKIAJHIE5PVHK473S2DA',
-    aws_secret_access_key: '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',
+    aws_access_key_id:     ENV["aws_access_key_id"],
+    aws_secret_access_key: ENV["aws_secret_access_key"],
     region:                'ca-central-1',
   }
-  config.fog_directory  = 'awsmaggie'
+  config.fog_directory  = ENV["fog_directory"]
   config.fog_public     = true
   config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
 end
