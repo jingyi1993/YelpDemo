3680d909625add0e55fd16986f180c62799fda52
马静怡 <472382917@qq.com>
2019-11-18 11:14:59 -0500

    use minigagick gem

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,4 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  process resize_to_fill: [200, 200]
   storage :fog
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
