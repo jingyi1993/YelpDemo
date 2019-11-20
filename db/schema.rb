9267286ae875188b48bdd6e72e5f841457ed601a
马静怡 <472382917@qq.com>
2019-11-19 20:46:03 -0500

    add user_id column to review table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_170511) do
+ActiveRecord::Schema.define(version: 2019_11_20_013144) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_11_19_170511) do
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
